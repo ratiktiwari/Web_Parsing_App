bdf2e8147ac4b7054c514d31329c1a042c589f6c
Ratik <ratiktiwari@gmail.com>
2020-07-24 19:47:18 +0530

    Parsed JSON single Object

--- app/src/main/java/com/example/webparsingapp/MainActivity.java
+++ app/src/main/java/com/example/webparsingapp/MainActivity.java
@@ -3,12 +3,52 @@ package com.example.webparsingapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    //URL - https://jsonplaceholder.typicode.com/todos/1
+
+    private RequestQueue requestQueue;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        requestQueue = Volley.newRequestQueue(this);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,
+                "https://jsonplaceholder.typicode.com/todos/1",
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        Log.d("JSON:", "onResponse: " + response);
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                        Log.d("ERROR:", "onErrorResponse: " + error);
+
+                    }
+                });
+
+        requestQueue.add(jsonObjectRequest);  // added the jsonObjectRequest task to background queue
     }
 }
